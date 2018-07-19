07770b4cdb61481f7a32796ceacdb4afcfac3707
helloJun <641380205@qq.com>
2018-07-19 20:14:59 +0800

    增加策略模块

--- app/src/main/java/com/tseenola/jijin/myjijing/biz/setting/SettingAty.java
+++ app/src/main/java/com/tseenola/jijin/myjijing/biz/setting/SettingAty.java
@@ -2,6 +2,7 @@ package com.tseenola.jijin.myjijing.biz.setting;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.tseenola.jijin.myjijing.R;
 
@@ -14,6 +15,7 @@ public class SettingAty extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("vbvb", "onCreate: ");
         setContentView(R.layout.activity_setting);
     }
 }
