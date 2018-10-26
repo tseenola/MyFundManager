2672e45ee9d8353a427c5a74600f524828404891
tseenola <641380205@qq.com>
2018-10-27 00:32:21 +0800

    增加 boll线回测策略

--- app/src/main/java/com/tseenola/jijin/myjijing/biz/huobi/view/BollBackTestAty.java
+++ app/src/main/java/com/tseenola/jijin/myjijing/biz/huobi/view/BollBackTestAty.java
@@ -28,6 +28,8 @@ public class BollBackTestAty extends LineAty {
     @Bind(R.id.line_chart)
     LineChartView lineChart;
 
+    private static final int STATUS_NULL = 0;//非持有状态
+    private static final int STATUS_HOLD = 1;//持有状态
     @Override
     public void initView() {
         setContentView(R.layout.activity_boll_back_test);
@@ -36,15 +38,66 @@ public class BollBackTestAty extends LineAty {
 
     @OnClick(R.id.bt_RunBackTest)
     public void onViewClicked() {
-        /**
-         * boll线策略1 ，
-         * 1.收盘价从下穿过上轨，买进。
-         * 2.收盘价从上穿过中轨，卖出
-         */
+
+        bollStdStrategy();
+
+
+
+    }
+
+    /**
+     * boll线策略2.---买入投机，卖出稳健型
+     * 1.非持有状态下-收盘价从下穿过下轨，买入
+     * 2.持有状态下-收盘价从上穿过下轨道，卖出（防止继续跌）
+     * 3.持有状态下-收盘价从下穿过中轨，卖出。
+     */
+    private void bollStdStrategy2() {
         double preCloseVal = 0d;
         StringBuilder info = new StringBuilder("");
+        int curStatus = STATUS_NULL;
+        double shouYiRateSum = 0;//收益率
+        double curHoldVal = 0d;//当前持有价格
         for (int i = 0; i < mPointValues_Y.size(); i++) {
+            double closeVal = mPointValues_Y.get(i).getY();
+            double bollUpVal = mPointValues_Y_BollUp.get(i).getY();
+            double bollDownVal = mPointValues_Y_BollLow.get(i).getY();
+            double avgVal = mPointValues_Y_Avg.get(i).getY();
+            if (i == 0) {
+                preCloseVal = closeVal;
+            }
 
+            if (closeVal > bollDownVal && preCloseVal < bollDownVal && curStatus ==STATUS_NULL) {//1.收盘价从下穿过下轨
+                tvInfo.append("买入："+i +" close:"+closeVal + "\n");
+                curStatus = STATUS_HOLD;
+                curHoldVal = closeVal;
+            } else if(closeVal < bollDownVal && preCloseVal > bollDownVal && curStatus == STATUS_HOLD){//持有状态下-收盘价从上穿过下轨道，卖出（防止继续跌）
+                double curShouYiRate = (closeVal-curHoldVal)/curHoldVal;
+                shouYiRateSum += curShouYiRate;
+                tvInfo.append("卖出："+i + " close:"+closeVal + "收益率："+curShouYiRate * 100 + "%\n");
+                curStatus = STATUS_NULL;
+            }else if (closeVal > avgVal && preCloseVal < avgVal && curStatus == STATUS_HOLD) {//3.收盘价从下穿过中轨，卖出
+                double curShouYiRate = (closeVal-curHoldVal)/curHoldVal;
+                shouYiRateSum += curShouYiRate;
+                tvInfo.append("卖出："+i + " close:"+closeVal + "收益率："+curShouYiRate * 100 + "%\n");
+                curStatus = STATUS_NULL;
+            }
+            preCloseVal = closeVal;
+        }
+        tvInfo.append("收益率："+shouYiRateSum * 100 +"%\n");
+    }
+
+    /**
+     * boll线策略1   买入稳健，卖出稳健型
+     * 1.收盘价从下穿过上轨，买进。
+     * 2.收盘价从上穿过中轨，卖出
+     */
+    private void bollStdStrategy() {
+        double preCloseVal = 0d;
+        StringBuilder info = new StringBuilder("");
+        int curStatus = STATUS_NULL;
+        double shouYiRateSum = 0;//收益率
+        double curHoldVal = 0d;//当前持有价格
+        for (int i = 0; i < mPointValues_Y.size(); i++) {
             double closeVal = mPointValues_Y.get(i).getY();
             double bollUpVal = mPointValues_Y_BollUp.get(i).getY();
             double avgVal = mPointValues_Y_Avg.get(i).getY();
@@ -52,19 +105,19 @@ public class BollBackTestAty extends LineAty {
                 preCloseVal = closeVal;
             }
 
-            if (closeVal > bollUpVal && preCloseVal < closeVal) {//1.收盘价从下穿过上轨
+            if (closeVal > bollUpVal && preCloseVal < closeVal && curStatus ==STATUS_NULL) {//1.收盘价从下穿过上轨
                 tvInfo.append("买入："+i +" close:"+closeVal + "\n");
-            } else if (closeVal < avgVal && preCloseVal > closeVal) {//2.收盘价从上穿过中轨
-                tvInfo.append("卖出："+i + " close:"+closeVal + "\n");
+                curStatus = STATUS_HOLD;
+                curHoldVal = closeVal;
+            } else if (closeVal < avgVal && preCloseVal > closeVal && curStatus == STATUS_HOLD) {//2.收盘价从上穿过中轨
+                double curShouYiRate = (closeVal-curHoldVal)/curHoldVal;
+                shouYiRateSum += curShouYiRate;
+                tvInfo.append("卖出："+i + " close:"+closeVal + "收益率："+curShouYiRate * 100 + "%\n");
+                curStatus = STATUS_NULL;
             }
-
+            preCloseVal = closeVal;
         }
-
-        /**
-         * boll线策略2.
-         * 1.收盘价从下穿过下轨，买入
-         * 2.
-         */
+        tvInfo.append("收益率："+shouYiRateSum * 100 +"%\n");
     }
 
     public static void launch (Context pContext, HistoryKLine pHistoryKLine){
